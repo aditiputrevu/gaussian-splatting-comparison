8c4198d185f69aa9eefa68e2290f69bf809746a4
Brendan <bpf2002@comcast.net>
2026-08-05 16:47:42 -0400

    Created main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+# Created by Brendan Fullerton on 2024-06-15.
+
+# Import statements
+
+
+def main():
+    print("Hello, World!")
+    # Main code goes here
+
+if __name__ == "__main__":
+    main()
